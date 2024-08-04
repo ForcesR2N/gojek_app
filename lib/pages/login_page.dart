f8ba8cb31d9435315dcdaedd91c6f2b133109c5b
ForcesR2N <muhammadzuhrizal07@gmail.com>
2024-08-04 23:46:38 +0700

    adding Carousel slier

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,80 +1,113 @@
 import 'package:flutter/material.dart';
 import 'package:loginpage_gojek/component/button.dart';
 import 'package:loginpage_gojek/utils/colors.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final List<String> imgList = [
+      'assets/gojek_image_1.png',
+      'assets/gojek_image_2.png',
+      'assets/gojek_image_3.png',
+    ];
+
     return Scaffold(
-      body: Stack(
+      body: Column(
         children: [
           Align(
             alignment: Alignment.topLeft,
             child: Padding(
-              padding: const EdgeInsets.all(16.0), // Optional padding to adjust position
+              padding: const EdgeInsets.all(16.0),
               child: Image.asset(
                 'assets/Gojek_logo_2022.png',
-                width: 100, // Set the desired width
-                height: 100, // Set the desired height
+                width: 100,
+                height: 100,
               ),
             ),
           ),
-          Center(
+          Expanded(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                MyButton(
-                  text: 'Masuk',
-                  onTap: () {},
+                SizedBox(height: 150),
+                CarouselSlider(
+                  options: CarouselOptions(
+                    height: 200.0,
+                    autoPlay: false,
+                    enlargeCenterPage: true,
+                  ),
+                  
+                  items: imgList
+                      .map((item) => Container(
+                            child: Center(
+                              child: Image.asset(item,
+                                  fit: BoxFit.cover, width: 1000),
+                            ),
+                          ))
+                      .toList(),
                 ),
-                const SizedBox(
-                  height: 15,
-                ),
-                MyButton(
-                  text: 'Belum ada akun?, Daftar dulu',
-                  onTap: () {},
-                  bgButton: Colors.white,
-                  txtColor: AppColor.buttonGreen,
-                  outlineColor: AppColor.buttonGreen,
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                const Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Wrap(
-                    alignment: WrapAlignment.start,
-                    runAlignment: WrapAlignment.center,
-                    spacing: 4.0,
-                    runSpacing: 4.0,
-                    children: [
-                      Text(
-                        "Dengan masuk atau mendaftar, Kamu menyetujui ",
-                      ),
-                      Text(
-                        "Ketentuan",
-                        style: TextStyle(
-                          color: AppColor.buttonGreen,
+                Expanded(
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        MyButton(
+                          text: 'Masuk',
+                          onTap: () {},
                         ),
-                      ),
-                      Text(
-                        "layanan",
-                        style: TextStyle(
-                          color: AppColor.buttonGreen,
+                        const SizedBox(
+                          height: 15,
                         ),
-                      ),
-                      Text(
-                        " dan ",
-                      ),
-                      Text(
-                        "Kebijakan privasi",
-                        style: TextStyle(
-                          color: AppColor.buttonGreen,
+                        MyButton(
+                          text: 'Belum ada akun?, Daftar dulu',
+                          onTap: () {},
+                          bgButton: Colors.white,
+                          txtColor: AppColor.buttonGreen,
+                          outlineColor: AppColor.buttonGreen,
                         ),
-                      ),
-                    ],
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Wrap(
+                            alignment: WrapAlignment.start,
+                            runAlignment: WrapAlignment.center,
+                            spacing: 4.0,
+                            runSpacing: 4.0,
+                            children: [
+                              Text(
+                                "Dengan masuk atau mendaftar, Kamu menyetujui ",
+                              ),
+                              Text(
+                                "Ketentuan",
+                                style: TextStyle(
+                                  color: AppColor.buttonGreen,
+                                ),
+                              ),
+                              Text(
+                                "layanan",
+                                style: TextStyle(
+                                  color: AppColor.buttonGreen,
+                                ),
+                              ),
+                              Text(
+                                " dan ",
+                              ),
+                              Text(
+                                "Kebijakan privasi",
+                                style: TextStyle(
+                                  color: AppColor.buttonGreen,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
