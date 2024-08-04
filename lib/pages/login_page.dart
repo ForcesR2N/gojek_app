4aec03478f6949c4a1d47c9c2b126a1e6ac83897
ForcesR2N <muhammadzuhrizal07@gmail.com>
2024-08-04 22:48:50 +0700

    adding image and fix little bug

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -8,65 +8,79 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            MyButton(
-              text: 'Masuk',
-              onTap: () {},
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            MyButton(
-              text: 'Belum ada akun?, Daftar dulu',
-              onTap: () {},
-              bgButton: Colors.white,
-              txtColor: AppColor.buttonGreen,
-              outlineColor: AppColor.buttonGreen,
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            const Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Wrap(
-                alignment: WrapAlignment.start,
-                runAlignment: WrapAlignment.center,
-                spacing: 4.0,
-                runSpacing: 4.0,
-                children: [
-                  Text(
-                    "Dengan masuk atau mendaftar, Kamu menyetujui ",
-                  ),
-                  Text(
-                    "Ketentuan",
-                    style: TextStyle(
-                      color: AppColor.buttonGreen,
-                    ),
-                  ),
-                   Text(
-                    "layanan",
-                    style: TextStyle(
-                      color: AppColor.buttonGreen,
-                    ),
-                  ),
-                  
-                  Text(
-                    " dan ",
-                  ),
-                  Text(
-                    "Kebijakan privasi",
-                    style: TextStyle(
-                      color: AppColor.buttonGreen,
-                    ),
-                  ),
-                ],
+      body: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0), // Optional padding to adjust position
+              child: Image.asset(
+                'assets/Gojek_logo_2022.png',
+                width: 100, // Set the desired width
+                height: 100, // Set the desired height
               ),
             ),
-          ],
-        ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                MyButton(
+                  text: 'Masuk',
+                  onTap: () {},
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                MyButton(
+                  text: 'Belum ada akun?, Daftar dulu',
+                  onTap: () {},
+                  bgButton: Colors.white,
+                  txtColor: AppColor.buttonGreen,
+                  outlineColor: AppColor.buttonGreen,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Wrap(
+                    alignment: WrapAlignment.start,
+                    runAlignment: WrapAlignment.center,
+                    spacing: 4.0,
+                    runSpacing: 4.0,
+                    children: [
+                      Text(
+                        "Dengan masuk atau mendaftar, Kamu menyetujui ",
+                      ),
+                      Text(
+                        "Ketentuan",
+                        style: TextStyle(
+                          color: AppColor.buttonGreen,
+                        ),
+                      ),
+                      Text(
+                        "layanan",
+                        style: TextStyle(
+                          color: AppColor.buttonGreen,
+                        ),
+                      ),
+                      Text(
+                        " dan ",
+                      ),
+                      Text(
+                        "Kebijakan privasi",
+                        style: TextStyle(
+                          color: AppColor.buttonGreen,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
